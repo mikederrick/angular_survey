2c2f21fccb93e79062bf449e9b18d006cf88f774
Mike Derrick <mike33d@gmail.com>
2020-05-08 12:20:07 -0400

    Initial commit
    Adding basic CRUD functionality (using uuid as the main identifier). In production,
    would definitely add an index here.

---
+++ SurveyStore.js
@@ -0,0 +1,71 @@
+const MongoClient              = require('mongodb').MongoClient;
+
+const SURVEYS_COLLECTION_KEY   = 'SURVEYS_COLLECTION_KEY';
+const MONGO_URL                = 'mongodb://localhost:27017';
+const MONGO_DB_NAME            = 'surveys_store';
+
+class Datastore {
+
+  connect() {
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(MONGO_URL, (err, client) => {
+
+        if (err) {
+          return reject(err);
+        } else {
+          this.client = client;
+          return resolve(client);
+        }
+      })
+    })
+  }
+
+  collection() {
+    return this.client.db(MONGO_DB_NAME).collection(SURVEYS_COLLECTION_KEY);
+  }
+
+  getSurveys() {
+    return this.collection().find({});
+  }
+
+  createSurvey(uuid, answers={}) {
+    return this.collection().insertOne({ 
+      uuid,
+      createdAt: new Date(),
+      answers: answers,
+      complete: false
+    })
+  }
+
+  deleteSurvey(uuid) {
+    return new Promise((resolve, reject) => {
+      this.collection().deleteOne({ uuid }, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      })
+    })
+  }
+
+  markSurveyComplete(uuid) {
+    return this.collection().updateOne({ uuid }, { $set: {
+        complete: true,
+        updatedAt: new Date()
+    }})
+  }
+
+  updateSurvey(uuid, answers) {
+    return this.collection().updateOne({ uuid }, { $set: {
+        answers: answers,
+        updatedAt: new Date()
+    }})
+  }
+
+  findSurvey(uuid) {
+    return this.collection().find({ uuid });
+  }
+}
+
+module.exports = Datastore;
---
+++ server.js
@@ -0,0 +1,102 @@
+const express                  = require('express');
+const app                      = express();
+const bodyParser               = require('body-parser');
+const SURVEY                   = require("./survey.json");
+const cookieParser             = require('cookie-parser');
+const uniqid                   = require('uniqid');
+const SurveyStore              = require('./SurveyStore');
+const surveyStore              = new SurveyStore();
+
+const SURVEY_KEY               = 'SURVEY_ID';
+
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.get("/api/questions", (req, res) => {
+  res.json(SURVEY)
+})
+
+app.get("/api/surveys", (req, res) => {
+  surveyStore.getSurveys().toArray((err, surveys) => {
+    if (err) {
+      res.status(400).json({error: err});
+    } else {
+      res.json(surveys);
+    }
+  })
+})
+
+app.delete("/api/surveys/:uuid", (req, res) => {
+  surveyStore.deleteSurvey(req.params.uuid).then((err,data) => {
+    res.json({success: !err})
+  })
+})
+
+app.post("/api/surveys", (req, res) => {
+  let surveyId = uniqid();
+  surveyStore.createSurvey(surveyId).then((err, result) => {
+    res.cookie(SURVEY_KEY, surveyId).json(result);
+  })
+})
+
+app.get("/api/surveys/current", (req, res) => {
+  let surveyId = req.cookies[SURVEY_KEY];
+  if (surveyId) {
+    surveyStore.findSurvey(surveyId).toArray((err, surveys) => {
+      if (surveys && surveys.length > 0) {
+        res.json({ survey: surveys[0] })
+      } else {
+        res.json({ survey: null })
+      }
+    })
+  } else {
+    res.json({ survey: null })
+  }
+})
+
+app.get("/api/surveys/:uuid", (req, res) => {
+  surveyStore.findSurvey(req.params.uuid).toArray((err, surveys) => {
+      if (surveys && surveys.length > 0) {
+        res.json({ survey: surveys[0] })
+      } else {
+        res.json({ survey: null })
+      }
+  })
+})
+
+app.put("/api/surveys/:uuid", (req, res) => {
+  surveyStore.updateSurvey(req.params.uuid, req.body).then((err, result) => {
+    res.json(result)
+  })
+})
+
+app.post("/api/surveys/current", (req, res) => {
+  let surveyId = req.cookies[SURVEY_KEY];
+  if (surveyId) {
+    surveyStore.updateSurvey(surveyId, req.body).then((err, result) => {
+      res.json(result)
+    })
+  } else {
+    let surveyId = uniqid();
+    surveyStore.createSurvey(surveyId, req.body).then((err, result) => {
+      res.cookie(SURVEY_KEY, surveyId).json(result)
+    })
+  }
+})
+
+app.post("/api/surveys/current/complete", (req, res) => {
+  let surveyId = req.cookies[SURVEY_KEY];
+  if (surveyId) {
+    surveyStore.markSurveyComplete(surveyId).then((err, result) => {
+      res.clearCookie(SURVEY_KEY).json(result)
+    })
+  } else {
+    res.status(400).json({success: false});
+  }
+})
+
+surveyStore.connect()
+           .then(() => {
+             app.listen(5000, () => console.log('Server running...'))
+           })
+           .catch(err => console.log(`Failed to start server: ${err}`));
